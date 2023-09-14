e4e6c87792035c2b5bd2ef790946abae06047e1c
Marcio Leandro Cristovam Azevedo de Holanda <marcioh.dev@gmail.com>
2023-09-14 14:21:54 -0300

    Criada pasta src e colocados todos os arquivos do projeto nela - falta adicionar quiz.html nela

---
+++ src/util.js
@@ -0,0 +1,35 @@
+
+import { bancoUsuarios } from './banco_de_dados.js';
+
+//<-- TELA DE PONTUAÇÃO -->
+function listarPontuacaoGeral() {
+
+
+    let texto = ''
+
+    //ordenando o banco de dados Usuários através de um sort
+    bancoUsuarios.sort((a, b) => b.pontuacao - a.pontuacao)
+
+    console.log(bancoUsuarios)//teste
+
+    for (let i = 0; i < bancoUsuarios.length; i++) {
+        texto += `${i + 1}º - ${bancoUsuarios[i].nome} - ${bancoUsuarios[i].pontuacao} PTS \n `
+    }
+
+    return `Escolha uma categoria: \n ${texto}`
+}
+
+function listarPontuacaoCategoria() {
+}
+
+function verificarCampoEmBranco(atributo) {//verifica se o nome recebido tem alguma coisa
+
+    if (atributo == 0) {
+        alert("Campo em branco. Tente novamente!")
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export { listarPontuacaoGeral, verificarCampoEmBranco, listarPontuacaoCategoria }
